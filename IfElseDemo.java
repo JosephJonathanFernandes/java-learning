a7954603438460688638359e4bda6d9a5aeb8cbd
Joseph <josephfernandes273@gmail.com>
2025-10-18 16:24:39 +0530

    first commit

---
+++ IfElseDemo.java
@@ -0,0 +1,20 @@
+class IfElseDemo {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+        int day = 3;
+       switch(day) {
+    case 1: System.out.println("Monday"); break;
+    case 2: System.out.println("Tuesday"); break;
+    default: System.out.println("Other day");
+}
+
+    }
+}

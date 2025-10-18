387e34d5f8001032cbfd349ec9573db36a785a67
Joseph <josephfernandes273@gmail.com>
2025-10-18 16:13:42 +0530

    first commit

---
+++ Practice.java
@@ -0,0 +1,20 @@
+class Add {
+    static void run() {
+        int a = 5, b = 3;
+        System.out.println("Sum: " + (a + b));
+    }
+}
+
+class EvenOdd {
+    static void run() {
+        int n = 10;
+        System.out.println((n % 2 == 0) ? "Even" : "Odd");
+    }
+}
+
+public class Practice {
+    public static void main(String[] args) {
+        Add.run();
+        EvenOdd.run();
+    }
+}

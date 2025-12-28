09ba6b1a215d61527b38d3a08649af0860d0e385
Joseph <josephfernandes273@gmail.com>
2025-12-28 13:51:25 +0530

    Fix unused local variable 'a' in ExceptionDemo.java

--- src/main/java/com/example/learningjava/examples/ExceptionDemo.java
+++ src/main/java/com/example/learningjava/examples/ExceptionDemo.java
@@ -5,7 +5,8 @@ public class ExceptionDemo {
   public static void main(String[] args) {
 
     try {
-      int a = 10 / 0;
+      int result = 10 / 0;
+      System.out.println(result);
     } catch (ArithmeticException e) {
       System.out.println("Cannot divide by zero!");
     } finally {
